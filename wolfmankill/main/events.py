29e9c95536eae0cc561d87cdbad222098625727e
Ruochen Shen <samisgenius@gmail.com>
2020-10-15 16:10:30 -0500

    Milestone 1 Done

--- wolfmankill/main/events.py
+++ wolfmankill/main/events.py
@@ -1,17 +1,40 @@
 from flask_socketio import emit
 from .. import socketio
+import threading
+from time import sleep
 
 values = {
     'slider1': 25,
     'slider2': 0,
 }
 
+
+threadLock = threading.Lock()
+
 @socketio.on('connect')
 def test_connect():
     emit('after connect',  {'data':'Lets dance'})
 
+
+global vote_counter
+vote_counter = 0
+global votes
+votes = {'player1':0, 'player2':0, 'player3':0, 'player4':0}
+
 @socketio.on('Slider value changed')
 def value_changed(message):
-    # values[message['who']] = message['data']
+    global votes
+    global vote_counter
+    votes[message['data']] += 1
     print(message)
-    # emit('update value', message, broadcast=True)
+    with threadLock:
+        vote_counter += 1
+        if vote_counter == 4:
+            vote_counter = 0
+            emit('update value', votes, broadcast=True)
+            votes = {'player1':0, 'player2':0, 'player3':0, 'player4':0}
+
+@socketio.on('done speaking')
+def done_speaking():
+    sleep(5)
+    emit('speak')
